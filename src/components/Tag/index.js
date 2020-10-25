700f2dbd26cf916b75ef810dedb0771d227705e8
Anton Sokolov <jetixc@gmail.com>
2020-10-26 02:50:20 +0300

    tag appearance

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -23,9 +23,17 @@ export const Tag = ({ name, identifier, onClick }) => {
 const Wrapper = styled.a`
   border: 1px solid ${props => props.theme.textColor};
   border-bottom: 1px solid ${props => props.theme.textColor}!important;
-  padding: 0 5px;
+  border-opacity: 0.1;
+  padding: 0px 6px;
   border-radius: 25px;
   background-color: ${props => props.active && props.theme.textColor};
   color: ${props => props.active && "#FFF!important"};
   cursor: pointer;
+
+  &:hover {
+    border: 1px solid rgba(0,0,0,0);
+    border-bottom: 1px solid rgba(0,0,0,0)!important;
+    color: ${props => props.theme.bgColor}!important;
+    background-color: ${props => props.theme.textColor};
+  }
 `;
